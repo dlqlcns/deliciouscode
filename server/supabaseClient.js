982eb0eef272a395286034b637c503e214885cc4
dlqlcns <panddo1605@gmail.com>
2025-11-22 17:03:30 +0900

    Update supabaseClient.js

--- server/supabaseClient.js
+++ server/supabaseClient.js
@@ -1,6 +1,6 @@
-// supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://wskxzuzyxnsywmyqeppt.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
