37c7936c5cf37636e2414d3274c8f121581af151
dlqlcns <panddo1605@gmail.com>
2025-11-23 00:14:34 +0900

    Delete server/routes/ai.js

--- server/routes/ai.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { generateRecipeList, generateRecipeDetail } from "../controllers/aiController.js";
-
-const router = express.Router();
-
-router.post("/list", generateRecipeList);
-router.post("/detail", generateRecipeDetail);
-
-export default router;
