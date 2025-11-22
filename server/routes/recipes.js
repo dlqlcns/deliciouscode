9ace952d4ef346fc1eab838d957169e94bee622a
dlqlcns <panddo1605@gmail.com>
2025-11-22 21:33:43 +0900

    Update recipes.js

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -1,9 +1,14 @@
 import express from 'express'
-import { getAllRecipes, getRecommendedRecipes } from '../controllers/recipesController.js'
+import {
+  getAllRecipes,
+  getRecommendedRecipes,
+  getRecipeById
+} from '../controllers/recipesController.js'
 
 const router = express.Router()
 
 router.get('/recommended', getRecommendedRecipes)
 router.get('/', getAllRecipes)
+router.get('/:id', getRecipeById) // 👈 상세 페이지용 라우트 추가
 
 export default router
