bcab57e394dec8c0507c4233ea111fe6684a07f8
dlqlcns <panddo1605@gmail.com>
2025-11-23 18:26:30 +0900

    Delete server/routes/food.js

--- server/routes/food.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getFoodInfo } from "../controllers/foodController.js";
-
-const router = express.Router();
-
-// /api/food-info?q=김치
-router.get("/food-info", getFoodInfo);
-
-export default router;
